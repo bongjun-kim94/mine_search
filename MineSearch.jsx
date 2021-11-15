df74dc8e746aa656d310780c928c57d8e95c1908
bongjun-kim94 <mydlwm_s@naver.com>
2021-11-15 16:43:42 +0900

    1115 추가

--- MineSearch.jsx
+++ MineSearch.jsx
@@ -1,9 +1,28 @@
-import React, { useReducer, createContext } from 'react';
+import React, { useReducer, createContext, useMemo } from 'react';
 import Table from './Table';
 import Form from './Form';
 
-const TableContext = createContext({
+export const CODE = {
+    MINE: -7,
+    NORMAL: -1,
+    QUESTION: -2,
+    FLAG: -3,
+    QUESTION_MINE: -4,
+    FLAG_MINE: -5,
+    CLICKED_MINE: -6,
+    OPENED: 0, // 0 이상이면 다 OPENED
+};
 
+// 다른 파일에서 쓸 수 있게 export
+export const TableContext = createContext({
+    tableData: [
+        [-1, -1, -1, -1, -1, -1, -1],
+        [-7, -1, -1, -1, -1, -1, -1],
+        [-1, -7, -1, -7, -7, -1, -1],
+        [],
+        [],
+    ],
+    dispatch: () => {},
 });
 
 const initialState= {
@@ -12,8 +31,15 @@ const initialState= {
     result: '',
 };
 
+export const START_GAME = 'START_GAME';
+
 const reducer = (state, action) => {
     switch (action.type) {
+        case START_GAME:
+            return {
+                ...state,
+                tableData: plantMine(action.row, action.cell, action.mine)
+            };
         default:
             return state;
     }
@@ -21,10 +47,11 @@ const reducer = (state, action) => {
 
 const MineSearch = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
+    const value = useMemo(() => ({ tableData: state.tableData, dispatch }), [state.tableData]);
 
     return (
         <>
-            <TableContext.Provider value={{ tableData: state.tableData, dispatch }}>
+            <TableContext.Provider value={value}>
                 <Form />
                 <div>{state.timer}</div>
                 <Table />
