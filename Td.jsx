dd7af73ad706547111e3fd418c7fc27db235d8e5
bongjun-kim94 <74786253+bongjun-kim94@users.noreply.github.com>
2021-11-17 02:36:59 +0900

    1117 추가

--- Td.jsx
+++ Td.jsx
@@ -2,9 +2,32 @@ import React, { useContext } from 'react';
 import { TableContext } from './MineSearch';
 
 const getTdStyle = (code) => {
+    switch (code) {
+        case CODE.NORMAL:
+        case CODE.MINE:
+            return {
+                background: '#444',
+            };
+        case CODE.OPENED:
+            return {
+                background: 'white',
+            };
+        default:
+            return {
+                background: 'white',
+            }
+    }
 };
 
 const getTdText = (code) => {
+    switch (code) {
+        case CODE.NORMAL:
+            return '';
+        case CODE.MINE:
+            return 'X';
+        default:
+            return '';
+    } 
 };
 
 const Td = ({ rowIndex, cellIndex }) => {
