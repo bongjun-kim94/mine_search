7cb6a6562194d7640a464f8f0158c33d2b668e1f
bongjun-kim94 <74786253+bongjun-kim94@users.noreply.github.com>
2021-11-16 17:22:26 +0900

    1116 추가

--- Td.jsx
+++ Td.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TableContext } from './MineSearch';
 
-const Td = () => {
+const getTdStyle = (code) => {
+};
 
-}
+const getTdText = (code) => {
+};
+
+const Td = ({ rowIndex, cellIndex }) => {
+    const { tableData } = useContext(TableContext);
+    return(
+        <>
+            <td style={getTdStyle(tableData[rowIndex][cellIndex])}
+            >
+                {getTdText(tableData[rowIndex][cellIndex])}
+            </td>
+        </>
+    );
+};
 
 export default Td;
